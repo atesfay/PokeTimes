4e701982d6e60228ea981d8b40afe13c4e753c0a
atesfay <abrahamkt@yahoo.com>
2019-09-26 23:59:00 -0700

    install read dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import { BrowserRouter, Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
+
   );
 }
 
