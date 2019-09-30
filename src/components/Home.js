98c615029f74980722cedcfc0f71e54353ff52dc
atesfay <abrahamkt@yahoo.com>
2019-09-29 22:27:11 -0700

    add Using Axios-2

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,11 +15,26 @@ class Home extends Component {
             })
     }
     render(){
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map( post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content" >
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
         return (
             <div>
                 <div className="container">
                     <h4 className="center">Home</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+                    {postList}
                 </div>
             </div>
         )
