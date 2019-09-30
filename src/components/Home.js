802d0a7c8132f2b62cddc8365636551f0723ed2a
atesfay <abrahamkt@yahoo.com>
2019-09-29 20:47:18 -0700

    add Using Axios-1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React,{ Component} from 'react'
+import axios from 'axios'
 
-const Home = () => {
-    return (
-        <div>
-            <div className="container">
-                <h4 className="center">Home</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+class Home extends Component {
+    state ={
+        posts:[]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(0,20)
+                })
+            })
+    }
+    render(){
+        return (
+            <div>
+                <div className="container">
+                    <h4 className="center">Home</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+   }
+
 }
 
 export default Home
