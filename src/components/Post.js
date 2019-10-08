5ede8895224e534410ee0101b59add1f3149ed24
atesfay <abrahamkt@yahoo.com>
2019-10-07 22:57:46 -0700

    Add Map Dispatch To Props DELETE PROPS

--- src/components/Post.js
+++ src/components/Post.js
@@ -19,11 +19,23 @@ class Post extends Component {
     //         //  console.log(res)
     //      })
     // }
+
+    handelClick = () => {
+        this.props.deletePost(this.props.post.id)
+        this.props.history.push('/');
+    }
+    
     render(){
+        console.log(this.props)// print deletePoste()
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="right">
+                    <button className="btn grey" onClick={this.handelClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ) :(
             <div className="center"> Loading post ...</div>
@@ -38,9 +50,15 @@ class Post extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     let id = ownProps.match.params.post_id;
-    return{
+    return {
         post: state.posts.find(post => post.id === id)
     }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id})}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,4 @@
+import Post from "../components/Post";
 
 const initState = {
     posts: [
@@ -7,58 +8,71 @@ const initState = {
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '2',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '3',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '4',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '5',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '6',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '7',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '8',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '9',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '10',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
-            id: '1',
+            id: '11',
             title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         }
     ]
 }
 const rootReducer = (state = initState, action) => {
+    
+    if (action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return {
+            ...state,
+            posts: newPosts
+           
+        }
+        
+    }
+    // console.log(action); // Print DELETE_POST
     return state;
 }
 
