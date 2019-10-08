1eb5550eae0dbf93f99c0a8cd978a4948657683b
atesfay <abrahamkt@yahoo.com>
2019-10-07 22:23:12 -0700

    Add Blog Detail Page

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,17 +3,57 @@ const initState = {
     posts: [
         {
             id: '1',
-            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
             id: '1',
-            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         },
         {
             id: '1',
-            title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
+            body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        },
+        {
+            id: '1',
+            title: 'Titile : sunt aut facere repellat ',
             body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
         }
     ]
